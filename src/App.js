4c27cf31a971687c21db4ed5e1199378df1bbb62
onkar raghunath shelke <onkarraghunathshelke@gmail.com>
2024-09-29 22:19:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>Face-recognition-Technology</h1>
+    </div>
+  );
+}
+
+export default App;
